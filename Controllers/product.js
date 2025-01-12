f07bc152232258acd0ff3a92a87bd350c3213608
sari705 <sari125705@gmail.com>
2025-01-12 12:12:47 +0200

    הוספת בדיקות תקינות לעדכון מוצר

--- Controllers/product.js
+++ Controllers/product.js
@@ -148,81 +148,33 @@ export async function deleteProduct(req, res) {
 }
 
 export async function updateProduct(req, res) {
-    let { id } = req.params;
+    let { id } = req.params;    
     let { body } = req;
-    let isErr = false;
-    let err;
-    if (!body.name) {
-        err += "name, ";
-        isErr = true;
-    }
+    
 
-    if (!body.price) {
-        err += "price, ";
-        isErr = true;
-    }
-
-    if (!body.description) {
-        err += "description, ";
-        isErr = true;
-    }
-
-    if (!body.images) {
-        err += "images, ";
-        isErr = true;
-    }
-
-    if (!body.stock) {
-        err += "stock, ";
-        isErr = true;
-    }
-
-    if (!body.tag) {
-        err += "tag, ";
-        isErr = true;
-    }
-
-    if (!body.categories) {
-        err += "categories ";
-        isErr = true;
-    }
-
-    if (isErr) {
-        return res.status(404).json({
-            title: "missing detailes",
-            message: err + "required",
-        });
-    }
-
-    if (body.name.length < 3) {
+    if (body.name?.length < 3) {
         return res.status(400).json({
             title: "detailes are not correct",
             message: "the product name is too short",
         });
     }
 
-    if (body.price<0) {
+    if (body.price&&body.price<0) {
         return res.status(400).json({
             title: "detailes are not correct",
             message: "the product price is too low",
         });
     }
 
-    if (body.stock<0) {
+    if (body.stock&&body.stock<0) {
         return res.status(400).json({
             title: "detailes are not correct",
             message: "the product stock is too low",
         });
     }
 
-    if (!body.categories.length) {
-        return res.status(400).json({
-            title: "detailes are not correct",
-            message: "the product categories is empty",
-        });
-    }
 
-    if (!body.images.length) {
+    if (body.images?.length==0) {
         return res.status(400).json({
             title: "detailes are not correct",
             message: "the product images is empty",
