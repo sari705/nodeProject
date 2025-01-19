8c3cffa384eb1ea134942178f1a22ac2eead5341
Biny <rachelfa33@gmail.com>
2025-01-19 23:29:43 +0200

    add biny

--- Controllers/product.js
+++ Controllers/product.js
@@ -148,6 +148,7 @@ export async function deleteProduct(req, res) {
 }
 
 export async function updateProduct(req, res) {
+    let biny = 5;
     let { id } = req.params;    
     let { body } = req;
     
