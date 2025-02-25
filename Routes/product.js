fc0e3a87194e1a7f0039b03018f672a4e650f412
sari705 <sari125705@gmail.com>
2025-02-25 14:36:42 +0200

    add getProductsByCategory function

--- Routes/product.js
+++ Routes/product.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
-import { addProduct, deleteProduct,getAllProducts, getProduct, updateProduct, getTotalPages } from "../Controllers/product.js"
+import { addProduct, deleteProduct,getAllProducts, getProduct, updateProduct, getTotalPages, getProductsByCategory } from "../Controllers/product.js"
 
 
 const router = Router()
 router.get("/", getAllProducts)
 router.get("/pages", getTotalPages)
 router.get("/:id", getProduct)
-router.post("/category", getProduct)
+router.post("/category", getProductsByCategory)
 router.delete("/:id", deleteProduct)
 router.put("/:id", updateProduct)
 router.post("/", addProduct)
