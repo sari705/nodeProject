e4400907c3d3270c173425082de6a423dd14fd42
Biny <rachelfa33@gmail.com>
2025-03-09 00:14:14 +0200

    add log

--- middlewares/IdTest.js
+++ middlewares/IdTest.js
@@ -10,7 +10,7 @@ export function checkMiddlware(req, res, next) {
     try {
         let result = jwt.verify(token, "baby"
             //  process.env.SECRET_KEY
-            )
+        )
         console.log(result);
         next();
 
@@ -28,10 +28,11 @@ export function checkManager(req, res, next) {
         return res.status(401).json({ title: "משתמש לא מזוהה", message: "עליך לבצע כניסה קודם" });
     token = token.split(" ")[1];
     try {
-        let result = jwt.verify(token,"baby"
+        let result = jwt.verify(token, "baby"
             //  process.env.SECRET_KEY
-            )
-        console.log("role: ", result.role); 
+        )
+        console.log("Decoded JWT:", result);
+        console.log("role: ", result.role);
         if (result.role == "MANAGER")
             next();
         return res.status(403).json({ title: "forbidden", message: "אינך מורשה לפעולה זו" })
