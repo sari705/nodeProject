bf7621496afdb77708a442bcae18e864ba481cb6
sari705 <sari125705@gmail.com>
2025-03-09 15:31:20 +0200

    Add jwt

--- Models/product.js
+++ Models/product.js
@@ -1,6 +1,7 @@
 import { Schema, model } from "mongoose"
 import Categories from "../utils/categories.js"
 import TagsEnum from "../utils/tags.js"
+import Joi from "joi" 
 
 
 const productSchema = Schema({
@@ -20,5 +21,17 @@ const productSchema = Schema({
         enum: TagsEnum // הגדרה כ-enum מתוך TagsEnum
     }
 })
+
+
+
+export const validateSchema = Joi.object({
+    name: Joi.string().min(3).required(),
+    price: Joi.number().min(0).required(),
+    description: Joi.string().required(),
+    images: Joi.array().items(Joi.string().uri()).min(1).required(),
+    stock: Joi.number().integer().min(1).required(),
+    tag: Joi.string().required(),
+    categories: Joi.array().items(Joi.string()).min(1).required()
+});
 
 export const productModel = model("product", productSchema)
