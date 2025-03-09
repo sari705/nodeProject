1bc640a6c9ef1c9cf16567771abcee5e61810811
sari705 <sari125705@gmail.com>
2025-03-09 15:58:26 +0200

    Fix check manager

--- middlewares/IdTest.js
+++ middlewares/IdTest.js
@@ -3,17 +3,16 @@ import jwt from "jsonwebtoken";
 export function checkMiddlware(req, res, next) {
     let token = req.headers.authorization;////מה שמגיע בבקשה ב haeders
     console.log("token in check: ", token);
-    
-    if (!token)
+
+    if (!token || !token.startsWith("Bearer "))
         return res.status(401).json({
             title: "משתמש לא מזוהה", message: "עליך לבצע כניסה"
         });
     token = token.split(" ")[1];
     try {
-        let result = jwt.verify(token, "baby"
-            //  process.env.SECRET_KEY
-        )
+        let result = jwt.verify(token, "baby")
         console.log(result);
+        req.user = result;
         next();
 
     }
@@ -28,27 +27,36 @@ export function checkManager(req, res, next) {
 
     let token = req.headers.authorization;
     console.log(token);
-    
-    if (!token)
-        return res.status(401).json({ title: "משתמש לא מזוהה", message: "עליך לבצע כניסה קודם" });
+
+    if (!token || !token.startsWith("Bearer "))
+        return res.status(401).json({
+            title: "משתמש לא מזוהה",
+            message: "עליך לבצע כניסה קודם"
+        });
+
     token = token.split(" ")[1];
     console.log("token: ", token);
 
     try {
-        let result = jwt.verify(token, "baby"
-            //  process.env.SECRET_KEY
-        )
+        let result = jwt.verify(token, "baby");
         console.log("Decoded JWT:", result);
         console.log("role: ", result.role);
+
         if (result.role == "MANAGER") {
+            req.user = result;
             return next();
         }
-        return res.status(403).json({ title: "forbidden", message: "אינך מורשה לפעולה זו" })
+        return res.status(403).json({
+            title: "forbidden",
+            message: "אינך מורשה לפעולה זו"
+        })
     }
     catch (err) {
         console.log(err);
-
-        return res.status(401).json({ title: " משתמש לא מזוהה או שהמשתמש אינו בתוקף ", message: "עליך לבצע כניסה" });
+        return res.status(401).json({
+            title: " משתמש לא מזוהה או שהמשתמש אינו בתוקף ",
+            message: "עליך לבצע כניסה"
+        });
     }
 }
 
