8dd1747cc10fd77468affde6ab61d48528775222
Biny <rachelfa33@gmail.com>
2025-03-11 18:17:26 +0200

    fix orderSchema

--- Controllers/order.js
+++ Controllers/order.js
@@ -1,6 +1,6 @@
 import { orderModel } from "../Models/order.js";
-import {userModel} from "../Models/user.js";
-import mongoose from "mongoose"; 
+import { userModel } from "../Models/user.js";
+import mongoose from "mongoose";
 
 export async function getAllOrders(req, res) {
     try {
@@ -18,7 +18,7 @@ export async function getAllOrders(req, res) {
 export async function getOrderByUser(req, res) {
     try {
         let { id } = req.params;
-        let data = await orderModel.find({userId:id})
+        let data = await orderModel.find({ userId: id })
         if (!data) {
             return res.status(404).json({
                 title: "can`t get by id",
@@ -36,55 +36,58 @@ export async function getOrderByUser(req, res) {
 }
 
 //good
+// minimalProduct
 export const addOrder = async (req, res, next) => {
     let { body } = req;
-    if (!body.userId || !body.adress || !body.productList || body.productList.length == 0)
-      return res.status(404).json({
-        title: "missing body data",
-        message: "adress, userId ,productList are required",
-      });
-    for (let j = 0; j < body.productList.length; j++) {
-      if (!body.productList[j].price || !body.productList[j]._id)
+    if (!body.userId || !body.address || !body.minimalProduct || body.minimalProduct.length == 0)
         return res.status(404).json({
-          title: "missing body data",
-          message: "_id and price in productList are required",
+            title: "missing body data",
+            message: "address, userId ,minimalProduct are required",
         });
-      if (!body.productList[j].amount)
-        body.productList[j].amount = 1;
+    for (let j = 0; j < body.minimalProduct.length; j++) {
+        if (!body.minimalProduct[j].price || !body.minimalProduct[j]._id)
+            return res.status(404).json({
+                title: "missing body data",
+                message: "_id and price in minimalProduct are required",
+            });
+        if (!body.minimalProduct[j].amount)
+            body.minimalProduct[j].amount = 1;
     }
     try {
-      let user = await userModel.findById(body.userId);
-      if (!user)
-        return res.status(404).json({ title: "no such user", message: "userId not found" });
-  
-      let productids = body.productList.map(item => item._id);
-      let arrFullProducts = await productModel.find({ _id: { $in: productids } });
-      console.log(productids, arrFullProducts)///בדיקה בשבילי
-      if (arrFullProducts.length != productids.length)
-        return res.status(404)
-          .json({ title: "one or more products are invalid", message: "check if products are in store" });// היה רק כמה id ומצא לפיהם את המוצרים אם יש שלש id ורק שתי מוצרים מסיבה מסויימת ,למה שלא ימשיך קניה של שתי מוצרים ????? לסדר חשוב
-      //עלול להווצר כאן בעיה שאם מישהו הכניס מחיר לא נכון על מוצר . לדעתי צריך לקחת את המחיר מהמערך של המוצרים המלאים .השאלה היא אם הם שווים בסדרם
-      let cost = 0;
-      for (let i = 0; i < body.productList.length; i++)
-        cost += body.productList[i].price * body.productList[i].amount;
-      //לשים לב : לא שמרתי את המערך המלא בתוך ההזמנה רק השתמשתי איתו לעזרתי אם אני מחליטה לשמור על המוצרים המלאים בתוך כל הזמנה אני צריכה לשנות 
-      if (cost >= 200)
-        body.shippingPrice = 0;
-      else if (cost >= 100)
-        body.shippingPrice = 15;
-      else
-        body.shippingPrice = 30;
-  
-      body.finalPrice = cost + body.shippingPrice;
-  
-      let newOrder = new orderModel(body);
-      await newOrder.save();
-      res.json(newOrder);
+        let user = await userModel.findById(body.userId);
+        if (!user)
+            return res.status(404).json({ title: "no such user", message: "userId not found" });
+
+        let productids = body.minimalProduct.map(item => item._id);
+        let arrFullProducts = await productModel.find({ _id: { $in: productids } });
+        console.log(productids, arrFullProducts)///בדיקה בשבילי
+        if (arrFullProducts.length != productids.length)
+            return res.status(404)
+                .json({ title: "one or more products are invalid", message: "check if products are in store" });// היה רק כמה id ומצא לפיהם את המוצרים אם יש שלש id ורק שתי מוצרים מסיבה מסויימת ,למה שלא ימשיך קניה של שתי מוצרים ????? לסדר חשוב
+        //עלול להווצר כאן בעיה שאם מישהו הכניס מחיר לא נכון על מוצר . לדעתי צריך לקחת את המחיר מהמערך של המוצרים המלאים .השאלה היא אם הם שווים בסדרם
+        let cost = 0;
+        for (let i = 0; i < body.minimalProduct.length; i++)
+            cost += body.minimalProduct[i].price * body.minimalProduct[i].amount;
+        //לשים לב : לא שמרתי את המערך המלא בתוך ההזמנה רק השתמשתי איתו לעזרתי אם אני מחליטה לשמור על המוצרים המלאים בתוך כל הזמנה אני צריכה לשנות 
+        if (cost >= 200)
+            body.shippingPrice = 0;
+        else if (cost >= 100)
+            body.shippingPrice = 15;
+        else
+            body.shippingPrice = 30;
+
+        body.finalPrice = cost + body.shippingPrice;
+        body.date = new Date();
+        body.deadline.setDate(body.date.getDate() + 13);
+
+        let newOrder = new orderModel(body);
+        await newOrder.save();
+        res.json(newOrder);
     } catch (err) {
-      res.status(400).json({ title: "cannot save order", message: err.message });
+        res.status(400).json({ title: "cannot save order", message: err.message });
     }
-  };
-  
+};
+
 
 export async function deleteOrder(req, res) {
     let { id } = req.params;
@@ -118,7 +121,7 @@ export async function deleteOrder(req, res) {
 export async function updateOrder(req, res) {
     let { id } = req.params;
     try {
-        let data = await productModel.findByIdAndUpdate(id, {$set: {isSetOff:true}}, { new: true })
+        let data = await productModel.findByIdAndUpdate(id, { $set: { isSetOff: true } }, { new: true })
         if (!data) {
             return res.status("404").json({
                 title: "not found",
