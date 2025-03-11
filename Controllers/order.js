59163ea2d8ab6e6a882fe55f8370280059f0144a
Biny <rachelfa33@gmail.com>
2025-03-11 19:23:06 +0200

    fix addOrder

--- Controllers/order.js
+++ Controllers/order.js
@@ -79,8 +79,18 @@ export const addOrder = async (req, res, next) => {
             body.shippingPrice = 30;
 
         body.finalPrice = cost + body.shippingPrice;
+
         body.date = new Date();
-        body.deadline.setDate(body.date.getDate() + 13);
+        // body.deadline.setDate(body.date.getDate() + 13);
+
+        // Initialize body.deadline as a Date object if it's not already
+if (!(body.deadline instanceof Date)) {
+    body.deadline = new Date(body.deadline);
+}
+
+// Add 13 days to the deadline date
+body.deadline.setDate(body.deadline.getDate() + 13);
+
 
         let newOrder = new orderModel(body);
         await newOrder.save();
